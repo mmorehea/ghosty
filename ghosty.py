f43cb45a1ad12d8483075aa1990afcf6615f8e49
Drew Scatterday <ginja@Drews-MacBook-Pro.local>
2017-08-08 22:56:59 -0400

    Added scale ratio and fixed np index error with casting

--- ghosty.py
+++ ghosty.py
@@ -9,7 +9,7 @@ MAX_IMG_RESOLUTION = 2000
 
 with open('ROI2.csv') as f:
     line=f.readline()
-    
+
 
 header = line.strip().split(',')
 data = np.genfromtxt('ROI2.csv', delimiter=",", skip_header=1)
@@ -23,11 +23,32 @@ zdiff = max(zs) - min(zs)
 
 print "dimensions: " + str(xdiff) + ", " + str(ydiff) + ", " + str(zdiff)
 
+dim = [xdiff, ydiff, zdiff]
+
+maxDim = max(dim)
+print(maxDim)
+
+scaleRatio = MAX_IMG_RESOLUTION / maxDim
 
 
+xOut = xdiff * scaleRatio
+yOut = ydiff * scaleRatio
+zOut = zdiff * scaleRatio
+
+
+xOut = int(xOut)
+yOut = int(yOut)
+zOut = int(zOut)
+
+print(xOut,yOut,zOut)
+
+#For some reason it gave me an index error here so when I casted the values
+#as ints it worked great. Not sure if its okay to cast as int or not,
+#but it worked :)
 arr = np.zeros((xOut,yOut,zOut))
+
+print(arr)
 
 tifffile.imsave('nameOfFile.tif', arr)
 
 code.interact(local=locals())
-
